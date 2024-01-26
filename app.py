d0e3c4048fc1f359e2d9b39839fb135255d63ea8
Avinash <avinash.kumar@dizartsolutions.com>
2024-01-26 18:16:24 +0530

    page changes done

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request, jsonify
 import psycopg2
 import os
+from werkzeug.utils import secure_filename
+import tempfile
+import pandas as pd
+import psycopg2.extras
+
 
 app = Flask(__name__)
 
@@ -29,7 +34,7 @@ def add_data():
         # Application attributes
         father_name = request.form['father_name']
         roll_no = request.form['roll_no']
-        date_of_birth = request.form['date_of_birth']
+        date_of_birth = request.form['date_of_birth'] if request.form['date_of_birth'] else None
         email = request.form['email']
         pincode = request.form['pincode']
         application_no = request.form['application_no']
@@ -37,10 +42,20 @@ def add_data():
         conn = get_db_connection()
         cur = conn.cursor()
 
-        # Insert into applicant_details
-        cur.execute("INSERT INTO applicant_details (name, phone_number) VALUES (%s, %s) RETURNING id",
-                    (name, phone_number))
-        applicant_id = cur.fetchone()[0]
+        # Check if phone number exists
+        cur.execute("SELECT id FROM applicant_details WHERE phone_number = %s", (phone_number,))
+        result = cur.fetchone()
+
+        if result is None:
+            # Phone number doesn't exist, insert into applicant_details
+            cur.execute("INSERT INTO applicant_details (name, phone_number) VALUES (%s, %s) RETURNING id",
+                        (name, phone_number))
+            applicant_id = cur.fetchone()[0]
+        else:
+            # Phone number exists, use existing applicant_id
+            applicant_id = result[0]
+            cur.execute("UPDATE applicant_details SET name = %s WHERE id = %s", (name, applicant_id))
+
 
         # Insert into applicant_attributes
         cur.execute("INSERT INTO applicant_attributes (applicant_id, father_name, roll_no, date_of_birth, email, pincode, application_no) VALUES (%s, %s, %s, %s, %s, %s, %s)",
@@ -81,7 +96,7 @@ def get_data():
         if not query_parts:
             return jsonify({'error': 'No search criteria provided'}), 400
 
-        query = f"SELECT * FROM applicant_details ad JOIN applicant_attributes aa ON ad.id = aa.applicant_id WHERE {' OR '.join(query_parts)}"
+        query = f"SELECT distinct phone_number as number, name, ad.id as applicant_id FROM applicant_details ad JOIN applicant_attributes aa ON ad.id = aa.applicant_id WHERE {' OR '.join(query_parts)}"
 
         conn = get_db_connection()
         cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
@@ -96,5 +111,107 @@ def get_data():
         print(e)
         return jsonify({'status': 'error', 'message': str(e)}), 500
 
+@app.route('/profile/<applicant_id>')
+def profile(applicant_id):
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute("SELECT * FROM applicant_details ad JOIN applicant_attributes aa ON ad.id = aa.applicant_id WHERE ad.id = %s", (applicant_id,))
+        result = cur.fetchall()
+        cur.close()
+        conn.close()
+
+        summarized_result = {}
+        if result is None:
+            result = {'status': 'Error', 'message': 'No data for profile'}
+        else:
+            # Convert the result to a list of dictionaries
+            result = [dict(row) for row in result]
+
+            summarized_result["Name"] = result[0]['name']
+            summarized_result["Phone Number"] = result[0]['phone_number']
+            # Iterate through the result and modify attribute names
+            i = 1
+            for row in result:
+                summarized_result["Father's Name "+ str(i)] = row['father_name']
+                summarized_result["Roll Number "+ str(i)] = row['roll_no']
+                summarized_result["Date Of Birth "+ str(i)] = row['date_of_birth']
+                summarized_result["Email "+ str(i)] = row['email']
+                summarized_result["Pincode "+ str(i)] = row['pincode']
+                summarized_result["Application Number "+ str(i)] = row['application_no']
+                i += 1
+            # Flatten the dictionaries and add status key with value 'OK'
+            summarized_result["status"]= 'OK'
+            print(summarized_result)
+    except Exception as e:
+        summarized_result = {'status': 'Error', 'message': str(e)}
+    return render_template('profile.html', profile=summarized_result)
+
+def check_headers(data):
+            expected_headers = ['Application_No', 'Date_of_Birth', 'Roll_No', 'Candidate_Name', 'Gender', 'Father_Name', 'Area', 'Locality', 'City', 'State', 'PinCode', 'Mobile_Number', 'Email']
+            headers = data.columns.tolist()
+            if headers != expected_headers:
+                return False
+            return True
+
+@app.route('/bulk_import', methods=['POST'])
+def bulk_import():
+    try:
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        
+        # Save the file to a desired location
+        # if folder doesn't exist, create it
+        # take a temp path if upload_folder is not set
+        upload_folder = app.config.get('UPLOAD_FOLDER')
+        if not upload_folder:
+            upload_folder = tempfile.gettempdir()
+        
+        if not os.path.exists(upload_folder):
+            os.makedirs(upload_folder)
+        file.save(os.path.join(upload_folder, filename))
+        
+        # Read the data from the Excel/CSV file using pandas
+        if filename.endswith('.csv'):
+            data = pd.read_csv(os.path.join(upload_folder, filename))
+        elif filename.endswith('.xlsx'):
+            data = pd.read_excel(os.path.join(upload_folder, filename))
+        else:
+            raise ValueError('Invalid file format')
+        
+        if check_headers(data) == False:
+            raise ValueError('Invalid headers')
+        # Connect to the database
+        conn = get_db_connection()
+        cur = conn.cursor()
+        
+        # Iterate through the data and insert into the database
+        for index, row in data.iterrows():
+            # Check if phone number exists
+            cur.execute("SELECT id FROM applicant_details WHERE phone_number::bigint = %s", (row['Mobile_Number'],))
+            result = cur.fetchone()
+            applicant_id = None
+            if result is None:
+                cur.execute("INSERT INTO applicant_details (name, phone_number) VALUES (%s, %s)", (row['Candidate_Name'], row['Mobile_Number']))
+                cur.execute("SELECT lastval()")
+                applicant_id = cur.fetchone()[0]
+            else:
+                # Phone number exists, use existing applicant_id
+                applicant_id = result[0]
+                cur.execute("UPDATE applicant_details SET name = %s WHERE id = %s", (row['Candidate_Name'], applicant_id))
+                # Insert into applicant_details table
+                conn.commit()
+            
+            # Insert into applicant_attributes table
+            cur.execute("INSERT INTO applicant_attributes (applicant_id, father_name, roll_no, date_of_birth, email, pincode, application_no) VALUES (%s, %s, %s, %s, %s, %s, %s)", (applicant_id, row["Father_Name"], row["Roll_No"], row["Date_of_Birth"], row["Email"], row["PinCode"], row["Application_No"]))
+            conn.commit()
+        
+        # Close the database connection
+        cur.close()
+        conn.close()
+        return {'status': 'OK', 'message': 'File uploaded and data imported successfully'}, 200
+    except Exception as e:
+        return {'status': 'Error', 'message': str(e)}, 400
+        
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,36 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
   const bulkImportForm = document.getElementById("bulk-import-form");
   const addDataForm = document.getElementById("add-data-form");
+  const getDataForm = document.getElementById("filter-form");
+  const loader = document.getElementById("loader");
+  const overlay = document.getElementById("overlay");
   if (bulkImportForm) {
-    bulkImportForm.addEventListener("submit", function (event) {
+    bulkImportForm.addEventListener("submit",async function (event) {
       event.preventDefault(); // Prevent the default form submission behavior
 
-      // Handle the form submission here
-      // Use AJAX to send the form data to the server and update the page dynamically
-      const formData = new FormData(this);
-
-      fetch("/bulk_import", {
-        method: "POST",
-        body: formData,
-      })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-          return response.json(); // or response.text() if the server returns plain text
-        })
-        .then((data) => {
-          // Handle the response data here
-          console.log(data);
-          alert("Bulk import successful");
-        })
-        .catch((error) => {
-          // Handle any errors here
-          console.error(
-            "There has been a problem with your fetch operation:",
-            error
-          );
+      // Get the form data
+      const formData = new FormData(event.target);
+      // Send the AJAX request
+      try {
+        // Show loader
+        loader.style.display = "block";
+        overlay.style.display = "block";
+        const response = await fetch("/bulk_import", {
+          method: "POST",
+          body: formData,
         });
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json(); // or response.text() if the server returns plain text
+
+        // Handle the response data here
+        console.log(data);
+        alert("Data added successfully");
+      } catch (error) {
+        // Handle any errors here
+        console.error(
+          "There has been a problem with your fetch operation:",
+          error
+        );
+      } finally {
+        // Hide loader
+        loader.style.display = "none";
+        overlay.style.display = "none";
+      }
     });
   }
   if (addDataForm) {
@@ -43,6 +52,9 @@ document.addEventListener("DOMContentLoaded", function () {
       // Send the AJAX request
 
       try {
+        // Show loader
+        loader.style.display = "block";
+        overlay.style.display = "block";
         const response = await fetch("/add_data", {
           method: "POST",
           body: formData,
@@ -63,6 +75,91 @@ document.addEventListener("DOMContentLoaded", function () {
           "There has been a problem with your fetch operation:",
           error
         );
+      } finally {
+        // Hide loader
+        loader.style.display = "none";
+        overlay.style.display = "none";
+      }
+    });
+  }
+  if (getDataForm) {
+    getDataForm.addEventListener("submit", async function (event) {
+      event.preventDefault(); // Prevent the default form submission behavior
+
+      // Get the form data
+      const formData = new FormData(event.target);
+
+      // Send the AJAX request
+
+      try {
+        // Show loader
+        loader.style.display = "block";
+        overlay.style.display = "block";
+        const response = await fetch("/get_data", {
+          method: "POST",
+          body: formData,
+        });
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json(); // or response.text() if the server returns plain text
+        const tableData = data.data;
+        let table = document.getElementById("data-table");
+
+        // Clear the table
+        table.innerHTML = "";
+
+        // Create table header
+        let thead = document.createElement("thead");
+        let headerRow = document.createElement("tr");
+        [
+          ["applicant_id", "ID"],
+          ["name", "Name"],
+          ["number", "Phone Number"],
+        ].forEach(([key, header]) => {
+          let th = document.createElement("th");
+          th.textContent = header;
+          headerRow.appendChild(th);
+        });
+        thead.appendChild(headerRow);
+        table.appendChild(thead);
+
+        // Create table body
+        let tbody = document.createElement("tbody");
+        tableData.forEach((item) => {
+          let row = document.createElement("tr");
+          [
+            ["applicant_id", "ID"],
+            ["name", "Name"],
+            ["number", "Phone Number"],
+          ].forEach(([key, header]) => {
+            let td = document.createElement("td");
+            if (key === "applicant_id") {
+              // Create a hyperlink for the applicant_id
+              let a = document.createElement("a");
+              a.href = "/profile/" + item[key]; // Replace with the actual path
+              a.textContent = item[key];
+              td.appendChild(a);
+            } else {
+              td.textContent = item[key];
+            }
+            row.appendChild(td);
+          });
+          tbody.appendChild(row);
+        });
+        table.appendChild(tbody);
+      } catch (error) {
+        // Handle any errors here
+        console.error(
+          "There has been a problem with your fetch operation:",
+          error
+        );
+      } finally {
+        // Hide loader
+        loader.style.display = "none";
+        overlay.style.display = "none";
       }
     });
   }
